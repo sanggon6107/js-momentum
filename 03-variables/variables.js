c99ab7ae1aa1738bfd6197fd9a88a25058c57ae3
sanggon6107 <sanggon6107@naver.com>
2022-04-10 15:42:55 +0900

    const에 대하여

---
+++ 03-variables/variables.js
@@ -0,0 +1,10 @@
+// console.log();
+console.log(514315); // 브라우저의 console로 log를 보내서 출력하게 한다.
+console.log("hello, javascript!");
+
+// const
+const variable1 = 5;
+const variable2 = "const";
+console.log(variable1 + 1); // 6
+console.log(variable1 * 4); // 20
+console.log(variable2);
