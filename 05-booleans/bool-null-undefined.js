cb6403115cbdb7c5b1881a0e26c2e4c2818a1efe
sanggon6107 <sanggon6107@naver.com>
2022-04-10 21:32:34 +0900

    bool, null, undefined에 대하여

---
+++ 05-booleans/bool-null-undefined.js
@@ -0,0 +1,13 @@
+
+// bool
+let myFirstBoolean = true;
+myFirstBoolean = false;
+
+console.log(myFirstBoolean);
+
+// null
+const myFirstNull = null; // 내용물이 정의되어있고 그 내용물이 "nothing"인 것.
+
+// undefined
+let something; // undefined. 메모리상 잡혀있지만 그 내용물이 정의되어있지는 않다.
+               // undefined로 선언할 때는 반드시 const가 아니라 let이어야 한다.
