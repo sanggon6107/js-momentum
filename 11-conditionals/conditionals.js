d0379fcf8e976b10a5ffae38c6f3951f669574d0
sanggon6107 <sanggon6107@naver.com>
2022-04-14 22:40:53 +0900

    조건문 : 변수 타입 검사법

--- 11-conditionals/conditionals.js
+++ 11-conditionals/conditionals.js
@@ -1,8 +1,11 @@
 function noUnder20(){
     const age = prompt("How old are you?"); // 매우 옛날 방식. 해당 함수로 띄운 창은 css등으로 꾸밀 수 없어서 현재는 거의 쓰이지 않는다.
                                             // 유저에게 직접 물어보는 창을 띄운다.
-    if(age >=20){
+    if(parseInt(age) >=20){
         return true;
+    }
+    if (parseInt(age) == NaN){
+        return NaN;
     }
     else{
         return false;
