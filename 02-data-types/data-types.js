1c6e4595a753bd03f0675e6a5d5cf11fc175a84c
sanggon6107 <sanggon6107@naver.com>
2022-04-14 22:39:08 +0900

    타입체크 방법과 타입 변환 방법

--- 02-data-types/data-types.js
+++ 02-data-types/data-types.js
@@ -1,3 +1,7 @@
 2+2;         // int
 1.3 + 4.22;  // float
 "hello" + ", my name is..";     // str
+
+const myString = "123";
+console.log(typeof myString); // 타입 출력
+console.log(parseInt(myString)); // int로 파싱.
