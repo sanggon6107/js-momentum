b26f758451c330855b505e11fff3fc831011c7a4
sanggon6107 <sanggon6107@naver.com>
2022-04-14 22:36:33 +0900

    조건문

---
+++ 11-conditionals/conditionals.js
@@ -0,0 +1,15 @@
+function noUnder20(){
+    const age = prompt("How old are you?"); // 매우 옛날 방식. 해당 함수로 띄운 창은 css등으로 꾸밀 수 없어서 현재는 거의 쓰이지 않는다.
+                                            // 유저에게 직접 물어보는 창을 띄운다.
+    if(age >=20){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const result = noUnder20();
+
+console.log(result);
+
