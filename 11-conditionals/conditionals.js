b7bdc2c64858b3f5af99f28e0ef10cae262baf8f
sanggon6107 <sanggon6107@naver.com>
2022-04-14 22:46:10 +0900

    조건문

--- 11-conditionals/conditionals.js
+++ 11-conditionals/conditionals.js
@@ -1,10 +1,16 @@
+
+// 참고 : === vs ==
+// ==는 타입까지는 허용한다. 즉, 0 == '0', == [], 0 == false 주의 : "0" == [] 는 false이다.
+// ===는 타입까지도 철저하게 검사한다.
+
+//
 function noUnder20(){
     const age = prompt("How old are you?"); // 매우 옛날 방식. 해당 함수로 띄운 창은 css등으로 꾸밀 수 없어서 현재는 거의 쓰이지 않는다.
                                             // 유저에게 직접 물어보는 창을 띄운다.
     if(parseInt(age) >=20){
         return true;
     }
-    if (parseInt(age) == NaN){
+    if (parseInt(age) === NaN){
         return NaN;
     }
     else{
