ef06aeca1c99474e531927765d7d6a7670d24d44
sanggon6107 <sanggon6107@naver.com>
2022-04-20 22:07:27 +0900

    제출되어도 전체 페이지 새로고침 되지 않도록

--- app/app.js
+++ app/app.js
@@ -1,14 +1,10 @@
 const logInInput = document.querySelector("#logInForm input");
-const logInBtn = document.querySelector("#logInForm #logInBtn");
-const logInForm = document.querySelector("#onLogInSubmit"); // form을 받아온다.
+const logInForm = document.querySelector("#logInForm");
 
-function onClickLogInBtn(){
+function onLogInSubmit(event){
+    event.preventDefault();
     console.log("Hello, " + logInInput.value);
 }
 
-logInBtn.addEventListener("click", onClickLogInBtn);
 
-
-// submit은 기본적으로 브라우저가 새로고침 되게 만들어져있다.
-// 
-logInForm.addEventListener("submit", )
+logInForm.addEventListener("submit", onLogInSubmit)
