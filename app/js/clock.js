0dcdd17f537ca5dc163bf0975bc0541fee1d5cb3
sanggon6107 <sanggon6107@naver.com>
2022-04-26 22:06:57 +0900

    setInterval과 Date 객체로 시계 만들기

--- app/js/clock.js
+++ app/js/clock.js
@@ -1,6 +1,10 @@
-function sayHello()
+const clock = document.querySelector("#clock");
+
+function refreshClockState()
 {
-    console.log("hello world!");
+    const date = new Date();
+    clock.innerText =`${date.getHours() % 12} : ${date.getMinutes()} : ${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000); // 5초마다 inverval함수 호출한다.
+refreshClockState() // html이 뜨고 setInterval에 1초 넣었으므로 1초 후에 처음 함수 호출된다. 따라서 처음에 한번 호출한다.
+setInterval(refreshClockState, 1000); // 5초마다 inverval함수 호출한다.
