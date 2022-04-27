129a8b7ec16a37d7c2e02a334c9068b12439240f
sanggon6107 <sanggon6107@naver.com>
2022-04-27 22:33:31 +0900

    Date, padStart, String

--- 25-Date/Date.js
+++ 25-Date/Date.js
@@ -4,12 +4,12 @@ function refreshClockState()
 {
     const date = new Date(); // js에서는 class로 선언된 객체의 경우 new 키워드를 써야하는 것 같다. (동적 할당?)
                              // // Date객체는 시간 정보를 담고있다.
-    const hours = String(date.getHours % 12).padStart(2, "0"); // 숫자로 바꿀 때는 parseInt를 호출했지만, str타입으로
+    const hours = String(date.getHours() % 12).padStart(2, "0"); // 숫자로 바꿀 때는 parseInt를 호출했지만, str타입으로
                                                                // 파싱할 때는 String객체의 constructor에 집어넣는다.
                                                                // 연산자 재정의로 string타입 리턴하게 되어있나?
-    const minutes = String(date.getMinutes).padStart(2, "0");  // padStart : 첫 번째 인자만큼의 길이가 안될 때 두번째 인자로
+    const minutes = String(date.getMinutes()).padStart(2, "0");  // padStart : 첫 번째 인자만큼의 길이가 안될 때 두번째 인자로
                                                                // 오는 문자를 앞에 채워준다.
-    const seconds = String(date.getSeconds).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText =`${hours} : ${minutes} : ${seconds}`; 
 }
 
--- app/js/clock.js
+++ app/js/clock.js
@@ -3,7 +3,10 @@ const clock = document.querySelector("#clock");
 function refreshClockState()
 {
     const date = new Date();
-    clock.innerText =`${date.getHours() % 12} : ${date.getMinutes()} : ${date.getSeconds()}`;
+    const hours = String(date.getHours() % 12).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText =`${hours} : ${minutes} : ${seconds}`; 
 }
 
 refreshClockState() // html이 뜨고 setInterval에 1초 넣었으므로 1초 후에 처음 함수 호출된다. 따라서 처음에 한번 호출한다.
