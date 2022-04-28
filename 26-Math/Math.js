b4269807623911a201d2d7c094c6fbcc66d2c9e1
sanggon6107 <sanggon6107@naver.com>
2022-04-28 22:22:07 +0900

    Math

---
+++ 26-Math/Math.js
@@ -0,0 +1,58 @@
+const quoteList = [
+{
+    quote : "Be yourself; everyone else is already taken.",
+    author : "Oscar Wilde"
+},
+{
+    quote : "In three words I can sum up everything I've learned about life: it goes on.",
+    author : "Robert Frost"
+},
+{
+    quote : "If you tell the truth, you don't have to remember anything.",
+    author : "Mark Twain"
+},
+{
+    quote : "Without music, life would be a mistake.",
+    author : "Friedrich Nietzsche, Twilight of the Idols"
+},
+{
+    quote : "I solemnly swear that I am up to no good.",
+    author : "J.K. Rowling, Harry Potter and the Prisoner of Azkaban"
+},
+{
+    quote : "Love all, trust a few, do wrong to none.",
+    author : "William Shakespeare, All's Well That Ends Well"
+},
+{
+    quote : "Whenever I feel the need to exercise, I lie down until it goes away.",
+    author : "Paul Terry"
+},
+{
+    quote : "There is no friend as loyal as a book.",
+    author : "Ernest Hemingway"
+},
+{
+    quote : "One good thing about music, when it hits you, you feel no pain.",
+    author : "Bob Marley"
+},
+{
+    quote : "Do one thing every day that scares you",
+    author : "Eleanor Roosevelt"
+},
+{
+    quote : "Some infinities are bigger than other infinities.",
+    author : "John Green, The Fault in Our Stars"
+}
+];
+
+// Math : 기본적으로 내장되어있다. Math.PI : 파이.
+// Math.random() : [0, 1] 범위의 랜덤 수.
+// Math.floor(), Math.round(), Math.ceil() : 소수점 처리 함수들.
+
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const idx = Math.floor((Math.random() * quoteList.length))
+quote.innerText = quoteList[idx].quote;
+author.innerText = quoteList[idx].author;
